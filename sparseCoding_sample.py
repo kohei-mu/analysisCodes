1f5b1a68acc964d35d41176c6a288c9adab7b3df
kohei-mu <kendo7bharat@gmail.com>
2017-10-09 01:12:02 +0900

    modified

--- sparseCoding_sample.py
+++ sparseCoding_sample.py
@@ -10,48 +10,34 @@ from sklearn.decomposition import MiniBatchDictionaryLearning
 import pandas as pd
 from sklearn.decomposition import SparseCoder
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-f=open("toy.csv", "r")
-
+#f=open("toy.csv", "r")
 def file_reader(csv):
     df=pd.read_csv(csv,header=0)
     return df
+#data=file_reader(f).iloc[:,2]
 
-data=file_reader(f).iloc[:,2]
-
-t=data[0:100]
-sim=data[101:201]
+train=df.iloc[:,1][0:100]
+test=df.iloc[:,1][101:201]
 
 dico = MiniBatchDictionaryLearning(n_components=500, alpha=1, n_iter=1000,transform_algorithm = 'lasso_lars',transform_alpha=1.0, fit_algorithm = 'lars')  
-
-
-
-V = dico.fit(t).components_
-
-
-
+V = dico.fit(train).components_
 
 coder = SparseCoder(dictionary=V, transform_algorithm='omp', transform_n_nonzero_coefs=10)
-u = coder.transform(sim)
+u = coder.transform(test)
+reconstruction = np.dot(u, V)
 
+fig = plt.figure()
+plt.plot(train)
+fig = plt.figure()
+plt.plot(test)
+fig = plt.figure()
+plt.plot(reconstruction[0])
 
-s = np.dot(u, V)
+residual=(reconstruction[0]-test)**2
+fig = plt.figure()
+plt.plot(residual)
 
-
-
-
-
-
-
-t.plot()
-sim.plot()
-plt.plot(s[0])
-
-
-
-
-res=(s[0]-sim)**2
-plt.plot(res)
-
-import seaborn as sns
-sns.distplot(res)
+fig = plt.figure()
+sns.distplot(residual)
